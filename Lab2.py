8376372bc114baacebd84454550620b552bbc0e1
CALEB <CALEB.23@ichat.sp.edu.sg>
2024-05-05 15:52:44 +0800

    Lab2.py patch

--- Lab2.py
+++ Lab2.py
@@ -3,39 +3,44 @@ def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32):")
 
 def get_user_input():
-    inpu=input()
-    inpu=inpu.split(", ")
-    global inputt
-    inputt=[float(i) for i in inpu]
-    print(inputt)
+    input_list=input()
+    input_list=input_list.split(", ")
+    float_list=[float(i) for i in input_list]
+    print(float_list)
+    return float_list
 
-def calc_average():
-    average=sum(inputt)/len(inputt)
+def calc_average(x):
+    average=sum(x)/len(x)
     print ("the average is " + str(average))
+    return x
 
-def find_min_max():
-    minimum=min(inputt)
-    maximum=max(inputt)
+def find_min_max(x):
+    minimum=min(x)
+    maximum=max(x)
+    min_and_max = [minimum, maximum]
     print("the minimum temperature is " + str(minimum) + " and the maximum temperature is " + str(maximum))
+    return min_and_max
 
-def sort_temperature():
-    inputt.sort()
-    print(inputt)
+def sort_temperature(x):
+    x.sort()
+    print(x)
+    return x
 
-def calc_median_temperature():
-    length=len(inputt)
-    if (len(inputt)%2==0):
-        middle1=inputt[length//2]
-        middle2=inputt[(length//2)-1]
+def calc_median_temperature(x):
+    length=len(x)
+    if (len(x)%2==0):
+        middle1=x[length//2]
+        middle2=x[(length//2)-1]
         median=(middle1+middle2)/2
     else:
-        median=inputt[length//2]
+        median=x[length//2]
     print("the median temperature is " + str(median))
+    return median
 
 
 display_main_menu()
-get_user_input()
-calc_average()
-find_min_max()
-sort_temperature()
-calc_median_temperature()
+num_list=get_user_input()
+calc_average(num_list)
+find_min_max(num_list)
+sort_temperature(num_list)
+calc_median_temperature(num_list)
